421948fee8be5dbab9b779ec21209305c38b1f2c
PedroLucchese <pedro130300@gmail.com>
2021-04-09 15:43:18 -0300

    Tags and calc arrangement

--- js/principal.js
+++ js/principal.js
@@ -1,5 +1,30 @@
-console.log("Fui carregado de um externo");
-
 var titulo = document.querySelector(".titulo");
-
 titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+
+var pesoValido = true;
+var alturaValida = true;
+
+if(peso <= 0 || peso >= 1000){
+    pesoValido = false;
+    tdImc.textContent = "Peso inválido!";
+}
+
+if(altura <= 0 || altura >= 3.0){
+    alturaValida = false;
+    tdImc.textContent = "Altura inválida!";
+}
+
+if(alturaValida && pesoValido){
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
