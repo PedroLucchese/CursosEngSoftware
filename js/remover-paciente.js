0a62092d9c7cc65cc77052058a4be84a2c94b879
PedroLucchese <pedro130300@gmail.com>
2021-05-06 15:54:37 -0300

    Adding filter

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -3,7 +3,10 @@ var pacientes = document.querySelectorAll(".paciente");
 var tabela = document.querySelector("table");
 
     tabela.addEventListener("dblclick", function(event){
-        var alvoEvento = event.target;
-        var paiAlvo = alvoEvento.parentNode;
-        paiAlvo.remove();
+       
+       event.target.parentNode.classList.add("fadeOut");
+       
+       setTimeout(function(){
+        event.target.parentNode.remove(); 
+       },500);
     });
