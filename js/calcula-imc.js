1e07151adc8606555a117a95336d63ceb4217c56
PedroLucchese <pedro130300@gmail.com>
2022-10-18 20:18:43 -0300

    Page init

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -1,5 +1,5 @@
 var titulo = document.querySelector(".titulo");
-titulo.textContent = "Aparecida Nutricionista";
+titulo.textContent = "BACHARELADO EM ENGENHARIA DE SOFTWARE";
 
 var pacientes = document.querySelectorAll(".paciente");
 
