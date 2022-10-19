e6eb7496724953670e2e1aba85af976114699b7c
PedroLucchese <pedro130300@gmail.com>
2022-10-18 22:01:39 -0300

    trazendo array de cursos

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -1,25 +1,11 @@
 var botaoAdicionar = document.querySelector("#buscar-pacientes");
 
-botaoAdicionar.addEventListener("click", function() {
-    var xhr = new XMLHttpRequest();
+botaoAdicionar.addEventListener("click", function () {
+  console.log(curso.DISCIPLINAS[10].CODIGO);
+  
+  var cursos = JSON.parse(curso);
 
-    xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
-
-    xhr.addEventListener("load", function() {
-        var erroAjax = document.querySelector("#erro-ajax");
-
-        if (xhr.status == 200) {
-            erroAjax.classList.add("invisivel");
-            var resposta = xhr.responseText;
-            var pacientes = JSON.parse(resposta);
-
-            pacientes.forEach(function(paciente) {
-                adicionaPacienteNaTabela(paciente);
-            });
-        } else {
-            erroAjax.classList.remove("invisivel");
-        }
-    });
-
-    xhr.send();
+  cursos.forEach(function (paciente) {
+    adicionaCursoNaTabela(paciente);
+  });
 });
